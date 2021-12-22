f32b675c5c8dd19bf1101bad84e7ff5cb5a2b8cc
AngularProjects <ninawe.prachi@gmail.com>
2021-12-22 19:02:52 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,20 @@ export class AppComponent implements OnInit {
   loadedPosts = [];
   
   constructor(private http: HttpClient) {}
-  onCreatePost(postData: { title: string; content: string }) {}
+  ngOnInit() {
+    this.onFetchPosts();
+  }
+  
+  onCreatePost(postData: { title: string; content: string }) {
+  this.http
+      .post(
+        'http://34.135.235.174/user/retreivedata',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
   
   onFetchPosts() {
     // Send Http request
