3d1a89620781d281d59174c8127f8910a994ab95
AngularProjects <ninawe.prachi@gmail.com>
2021-12-22 12:37:47 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gke-angular';
+export class AppComponent implements OnInit {
+  //title = 'gke-angular';
+  loadedPosts = [];
+  
+  constructor(private http: HttpClient) {}
+  onCreatePost(postData: { title: string; content: string }) {}
+  
+  onFetchPosts() {
+    // Send Http request
+    this.http.get('http://34.135.235.174/user/retreivedata')
+    .subscribe(posts=>{
+      console.log(posts);
+    });
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+  
 }
